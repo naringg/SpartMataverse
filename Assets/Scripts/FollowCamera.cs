87693d64efeb4b1ddc60a58e8b1a0b98e1b4d137
Naring <0224kjh@gmail.com>
2025-02-19 19:15:37 +0900

    [feat]김지환 로비 NPC구현

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,6 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     public Transform target;
     float offsetX;
+    float offsetY;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,8 @@ public class FollowCamera : MonoBehaviour
             return;
 
         offsetX = transform.position.x - target.position.x;
-        
+        offsetY = transform.position.y - target.position.y;
+
     }
 
     // Update is called once per frame
@@ -25,6 +27,8 @@ public class FollowCamera : MonoBehaviour
 
         Vector3 pos = transform.position;
         pos.x = target.position.x + offsetX;
+        pos.y = target.position.y + offsetY;
+
         transform.position = pos;
     }
 }
