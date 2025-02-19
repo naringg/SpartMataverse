412f98c4d1b332b9cd3d921e7da7ee6a610f88b0
Naring <0224kjh@gmail.com>
2025-02-19 19:40:31 +0900

    [feat]김지환 씬 이동 버튼 제작

---
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   void ChangeSceneMinigame()
+    {
+        SceneManager.LoadScene("FlappyPlane");
+    }
+}
